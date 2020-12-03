4147a2f846c777b6664328bf532253bc5d6626df
shymine <timothee.neitthoffer@laposte.net>
2020-12-03 18:08:17 +0100

    init django commit

---
+++ engine/glauc/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GlaucConfig(AppConfig):
+    name = 'glauc'
--- engine/glauquengine/src/main/scala/glauquengine/model/Stage.scala
+++
@@ -1,9 +0,0 @@
-import Stage.COUNTER
-
-object Stage {
-    var COUNTER = 0
-}
-class Stage(var title: String, var description: String) {
-    val id = COUNTER
-    COUNTER+=1
-}
