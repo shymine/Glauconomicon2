da2a3913261b61638e3aa77c74dea56aa3e605a7
shymine <timothee.neitthoffer@laposte.net>
2021-02-24 18:03:41 +0100

    adding proxy for CORS

--- engine/glauc/views.py
+++ engine/glauc/views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
